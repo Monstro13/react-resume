c2e899b559b71d0786ecfacf2dd55111c57ce85d
Tcaregorodtcev Vadim <vcaregorodcev@gmail.com>
2016-12-13 12:10:38 +0300

    replace info

--- docs/components/about/index.jsx
+++ docs/components/about/index.jsx
@@ -6,11 +6,11 @@ const About = () => (
         <div className="about-title common-title">About me</div>
         <div className="about-text">
             <p className="redline">
-                Hello world! My name is <span className="joke">John Cena</span> Vadim. I'am web developer specializing in the development of client web applications with experience in more than three years.
+                Greetings! My name Vadim. I'm a web developer, specializing in the development of client web applications with more than three years experience.
             </p>
 
             <p className="redline">
-                I'am master student and now my specialty is ergonomic design and UI/UX development. Previously, when i was a bachelor, my specialization was software engineering. I have brought knowledge in the field of programming and development of information systems. I am confident that together these skills allow to create a truly multi-functional and user-friendly information systems.
+                I am currently a masters student, specialising in ergonomic design and UI/UX development. Previously, during my bachelor's degree, my specialization was in software engineering. I have a broad knowledge in the field of programming and development of information systems. I am confident that with my skills, i will be able to create a truly multi-functional and user-friendly information systems.
             </p>
         </div>
     </div>
